a20dfa9d6a70897fcc6678d4b178e10563623a8d
Stevie <stephenthomas43@gmail.com>
2021-11-13 14:34:28 -0800

    updates to streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -7,38 +7,47 @@ from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
 import json
 
-
+# Title of App
 st.title( "Crypto Prophet")
 
-
-option = st.sidebar.selectbox("What is your Risk Appetite?", ( 'Low', 'Medium', 'High'), 1)
+# This code gives us the sidebar on streamlit for the different dashboards
+option = st.sidebar.selectbox("Dashboards?", ( 'Google Trends', 'Coin Analysis', 'Charts'), 2)
 
 st.header(option)
 
-r = requests.get(f"https://api.alternative.me/v2/ticker/?limit=100")
-data = r.json()
-st.write(data)
+# This code gives us the "Coin" type-in tab, and we can nest analysis into the sidebar option
 symbol = st.sidebar.text_input("coin", value='BTC', max_chars=5)
+# This code gives us the Widget, for now its just an example but we can integrate into the Monte carlo simulation by nesting the code for Monte Carlo into this command.
 num_days = st.sidebar.slider('Amount to Invest', 1, 100000, 10)
-sorted(d.items(), key=lambda x: x[1], reverse=True)
 
-if option == 'Low':
-    st.header("Investors Risk Appetite is Low")
+# This is the Charts option on dashboard dropdown, and we can make it dynamic for each coin the api call gives us.
+if option == 'Charts':
+    st.header("Coin Charts")
+    st.image("https://finviz.com/crypto_charts.ashx?t=BTCUSD")
+
+
+# This is the Google Trends option on dashboard dropdown, and we can make it dynamic for each coin the api call gives us.    
+if option == 'Google Trends':
+    st.header("Google Trends")
+    st.image(f"https://trends.google.com/trends/explore?date=2012-01-01%202021-11-13&q=bitcoin")
+
+# This is the code for the Twitter API call and the query to do a full search of archives. The API is working but I can't figure out how to parse the info we want.   
+if option == 'Tweet Counts':
+    st.header("Tweet Counts")
+    tweepy_auth = tweepy.OAuthHandler("TWITTER_CONSUMER_KEY" , "TWITTER_CONSUMER_SECRET")
+    tweepy_auth.set_access_token("TWITTER_ACCESS_TOKEN", "TWITTER_ACCESS_TOKEN_SECRET")
+    api = tweepy.API(tweepy_auth)
     
-if option == 'Medium':
-    st.header("Investors Risk Appetite is Medium")
-    
-   
-if option == 'High':
-    st.header("Investors Risk Appetite is High")
-    data_obj = json.loads(data)
-    for i in range(50, 101):
-        coin_index = str(i)
-        st.write(data_obj[coin_index]['name'])
-        st.write(data_obj[coin_index]['symbol'])
-        
-sorted(d.items(), key=lambda x: x[1], reverse=True)
-    
+    public_tweets = api.search_full_archive("production", "BTC")
+    for tweet in public_tweets:
+        if "#BTC" in tweet.text:
+            words = tweet.text.split(' ')
+            for word in words:
+                if word.startswith('#') and word[1:].isalpha():
+                    symbol = word[1:]
+                    st.write(symbol)
+
+                    st.write(tweet.text)
 
     
        
