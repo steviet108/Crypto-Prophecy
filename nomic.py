d3a48bad8e84732efb88a8c9cf62ccfb1c58fa9f
cbsandiego <cbuted@gmail.com>
2021-11-14 01:15:11 -0800

    Cleaned data by adding  comments to each line of code.  Converted Price and Market Cap to reflect currency and updated Price Date to just date and removed empty time

--- nomic.py
+++ nomic.py
@@ -7,15 +7,14 @@ from dotenv import load_dotenv
 import requests
 import json
 import urllib.request
-from PIL import Image
-from IPython.display import Image, HTML
 import streamlit as st
 from st_aggrid import AgGrid
 
 # Load .env environment variables
 load_dotenv()
 
-st.header("Top 100 Cryptocurrencies by Market Cap")
+# Title of Dataframe
+st.subheader('Top 100 Cryptocurrencies by Market Cap')
 
 # Get nomics api key
 nomics_api_key = os.getenv("NOMICS_API_KEY")
@@ -28,19 +27,28 @@ nomics_df = pd.read_json(nomics_currency_url)
 # Create an empty DataFrame for top cryptocurrencies
 top_cryptos_df = pd.DataFrame()
 
-# Get rank, crytocurrency, price, 1d change, market cat, volume
+# Get rank, crytocurrency, price, price_date, market cap
 top_cryptos_df = nomics_df[['rank', 'logo_url', 'currency', 'name', 'price', 'price_date', 'market_cap']]
-# top_cryptos_df.set_index('rank', inplace=True)
 
-# convert your links to html tags 
-def path_to_image_html(logo_url):
-    return '<img src="'+ logo_url +'" width=50 >'
+# Rename column labels
+columns=['Rank', 'Logo', 'Symbol', 'Currency', 'Price (USD)', 'Price Date', 'Market Cap']
+top_cryptos_df.columns=columns
 
+# Set rank as index
+top_cryptos_df.set_index('Rank', inplace=True)
+
+# Convert text data type to numerical data type
+top_cryptos_df['Market Cap'] = top_cryptos_df['Market Cap'].astype('float64')
+
+# Convert Timestamp to date only
+top_cryptos_df['Price Date']=pd.to_datetime(top_cryptos_df['Price Date']).dt.date
+
+# Convert your links to html tags 
+def path_to_image_html(Logo):
+    return '<img src="'+ Logo +'" width=30 >'
+    
 # Display image in dataframe
-top_cryptos_df.logo_url = path_to_image_html(top_cryptos_df.logo_url)
+top_cryptos_df.Logo = path_to_image_html(top_cryptos_df.Logo)
 st.write(top_cryptos_df.to_html(escape=False), unsafe_allow_html=True)
 
-# Index by rank
 
-# Print top cryptos
-# AgGrid(top_cryptos_df, height=2850, fit_columns_on_grid_load=True)
