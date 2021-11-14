b5da55bbfde82bcd3fb25150cf94763e9aa4a588
Stevie <stephenthomas43@gmail.com>
2021-11-14 12:51:50 -0800

    update to streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -6,36 +6,64 @@ import os
 from dotenv import load_dotenv
 import alpaca_trade_api as tradeapi
 import json
+import plotly
+import tweepy
+load_dotenv()
 
-# Title of App
+# Header for main and sidebar
 st.title( "Crypto Prophet")
+st.sidebar.title("Options")
 
 # This code gives us the sidebar on streamlit for the different dashboards
-option = st.sidebar.selectbox("Dashboards?", ( 'Google Trends', 'Coin Analysis', 'Charts'), 2)
-
+option = st.sidebar.selectbox("Dashboards", ( 'Google Trends', 'Coin Analysis', ' Tweet Counts', 'Charts'), 2)
+#option_1 = st.sidebar.text_input("coin", value="{symbol}", max_chars=5)
+# This is the Header for each page
 st.header(option)
 
-# This code gives us the "Coin" type-in tab, and we can nest analysis into the sidebar option
-symbol = st.sidebar.text_input("coin", value='BTC', max_chars=5)
 # This code gives us the Widget, for now its just an example but we can integrate into the Monte carlo simulation by nesting the code for Monte Carlo into this command.
 num_days = st.sidebar.slider('Amount to Invest', 1, 100000, 10)
 
+
+
+
+if option == 'Coin Analysis':
+    st.header("Coin Analysis")
+    
+    symbol = st.sidebar.text_input("coin", value='BTC', max_chars=5)
+
+    #r = requests.get(f"URL{symbol}")
+
+
+
+
+
+
+
+
+
+
 # This is the Charts option on dashboard dropdown, and we can make it dynamic for each coin the api call gives us.
+# Make this function dynamic 
 if option == 'Charts':
     st.header("Coin Charts")
-    st.image("https://finviz.com/crypto_charts.ashx?t=BTCUSD")
+    #symbol = word[1:]
+    st.write({})
+    st.sidebar.text_input("coin", value='BTC', max_chars=5)
+
+    st.image("https://charts2.finviz.com/chart.ashx?t=AAPL")
 
 
 # This is the Google Trends option on dashboard dropdown, and we can make it dynamic for each coin the api call gives us.    
 if option == 'Google Trends':
     st.header("Google Trends")
-    st.image(f"https://trends.google.com/trends/explore?date=2012-01-01%202021-11-13&q=bitcoin")
+    #st.image(f"/Users/stephenthomas/Desktop/dashboard/Google_Trends.png")
+    st.image(f"/Users/stephenthomas/Desktop/dashboard/Google_Trends.png")
 
 # This is the code for the Twitter API call and the query to do a full search of archives. The API is working but I can't figure out how to parse the info we want.   
 if option == 'Tweet Counts':
     st.header("Tweet Counts")
-    tweepy_auth = tweepy.OAuthHandler("TWITTER_CONSUMER_KEY" , "TWITTER_CONSUMER_SECRET")
-    tweepy_auth.set_access_token("TWITTER_ACCESS_TOKEN", "TWITTER_ACCESS_TOKEN_SECRET")
+    tweepy_auth = tweepy.OAuthHandler("LSz4FuLR5xdgNwiUtQXvOimOW" , "ni7nB11naP8DL52QHxW7Bd7X291dZtj2gaNLJ5fi1WBgyYDFLI")
+    tweepy_auth.set_access_token("1365097854142849024-2llscaHopykWCU0xNf13kbK5Ic4lRE", "CdEfqzQvjF47AhhlolvEImeoRFvRtCXRhvEa7858RjlAI")
     api = tweepy.API(tweepy_auth)
     
     public_tweets = api.search_full_archive("production", "BTC")
